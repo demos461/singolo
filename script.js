624837bd42a6b8f6e2b35bb4fc299e9535cc5df9
Ilya Khrapko <iamdemos461@gmail.com>
2020-03-29 19:28:51 +0300

    feat: add active menu status, change screen size

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var works = document.querySelector('.works');
 var menu = document.getElementById('nav_id');
+var bmenu = document.querySelector('.menu__box');
 var tab = document.querySelector('.portfolio__buttons');
 var phone1 = document.querySelector('.verphone');
 var phone2 = document.querySelector('.horphone');
@@ -14,6 +15,25 @@ menu.addEventListener('click', (event) => {
         event.target.classList.add('nav_active');
     }
 });
+
+bmenu.addEventListener('click', (event) => {
+    if (event.target!=bmenu) 
+    {
+        bmenu.querySelectorAll('.menu__item').forEach(el => el.classList.remove('menu__item_active'));
+        event.target.classList.add('menu__item_active');
+    }
+});
+
+
+if (document.getElementById("menu__toggle").checked = true) {
+    document.querySelector("main").addEventListener('click', (event) => {
+        if (event.target!=bmenu) 
+        {
+            document.getElementById("menu__toggle").checked = false;
+        }
+    });
+}
+
 
 //SLIDER 
 
@@ -178,29 +198,40 @@ var portfolio = document.getElementById('portfolio').offsetTop-95;
 var about = document.getElementById('about').offsetTop-95;
 var contact = document.getElementById('contact').offsetTop-95;
 var navLink = document.querySelectorAll('.nav_link');
+var bmenuItem = document.querySelectorAll('.menu__item');
 
 window.addEventListener('scroll', function() {
     var page_offset=window.pageYOffset;
 
     if(page_offset>=0) {
         menu.querySelectorAll('a').forEach(el => el.classList.remove('nav_active'));
+        bmenuItem.forEach(el => el.classList.remove('menu__item_active'));
         navLink[0].classList.add('nav_active');
+        bmenuItem[0].classList.add('menu__item_active');
     }
     if(page_offset>=services && page_offset<portfolio) {
         menu.querySelectorAll('a').forEach(el => el.classList.remove('nav_active'));
+        bmenuItem.forEach(el => el.classList.remove('menu__item_active'));
         navLink[1].classList.add('nav_active');
+        bmenuItem[1].classList.add('menu__item_active');
     }
     if(page_offset>=portfolio && page_offset<about) {
         menu.querySelectorAll('a').forEach(el => el.classList.remove('nav_active'));
+        bmenuItem.forEach(el => el.classList.remove('menu__item_active'));
         navLink[2].classList.add('nav_active');
+        bmenuItem[2].classList.add('menu__item_active');
     }
     if(page_offset>=about && page_offset<contact) {
         menu.querySelectorAll('a').forEach(el => el.classList.remove('nav_active'));
+        bmenuItem.forEach(el => el.classList.remove('menu__item_active'));
         navLink[3].classList.add('nav_active');
+        bmenuItem[3].classList.add('menu__item_active');
     }
     if(page_offset>=contact) {
         menu.querySelectorAll('a').forEach(el => el.classList.remove('nav_active'));
+        bmenuItem.forEach(el => el.classList.remove('menu__item_active'));
         navLink[4].classList.add('nav_active');
+        bmenuItem[4].classList.add('menu__item_active');
     }
 
 });
